4b0dc9ceb9fa29376219ed6622c89cef82490f88
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-09 00:01:53 +0100

    💄 add styles

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import utilStyles from "../../styles/utils.module.css";
 import Layout from "../../components/layout";
 import Head from "next/head";
 import Date from "../../components/date";
@@ -24,13 +25,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      <Head>{postData.title}</Head>
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
